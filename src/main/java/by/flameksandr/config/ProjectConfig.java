f6761445840ac4f537ceb6e8872a20b22f7e7537
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2023-10-27 14:43:00 +0300

    add to the ProductRepository method addProduct

--- src/main/java/by/flameksandr/config/ProjectConfig.java
+++ src/main/java/by/flameksandr/config/ProjectConfig.java
@@ -1,6 +1,7 @@
 package by.flameksandr.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
@@ -10,6 +11,7 @@ import org.springframework.transaction.PlatformTransactionManager;
 import javax.sql.DataSource;
 
 @Configuration
+@ComponentScan(basePackages = {"by.flameksandr.repositories","by.flameksandr.services"})
 public class ProjectConfig {
 
     @Bean
---
+++ src/main/java/by/flameksandr/repositories/ProductRepository.java
@@ -0,0 +1,16 @@
+package by.flameksandr.repositories;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class ProductRepository {
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+    public void addProduct(String name) {
+        String sql = "insert into product values(null, ?)";
+        jdbcTemplate.update(sql, name);
+    }
+}
