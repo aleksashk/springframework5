1f720ee1d32e641eaf023a8ba2cd3782c22c054a
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2023-10-27 14:45:56 +0300

    add to the ProductService method addOneProduct

--- src/main/java/by/flameksandr/App.java
+++ src/main/java/by/flameksandr/App.java
@@ -1,12 +1,14 @@
 package by.flameksandr;
 
 import by.flameksandr.config.ProjectConfig;
+import by.flameksandr.services.ProductService;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class App {
     public static void main(String[] args) {
         try (var context = new AnnotationConfigApplicationContext(ProjectConfig.class)) {
-
+            ProductService p = context.getBean(ProductService.class);
+            p.addOneProduct();
         }
     }
 }
---
+++ src/main/java/by/flameksandr/services/ProductService.java
@@ -0,0 +1,17 @@
+package by.flameksandr.services;
+
+import by.flameksandr.repositories.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProductService {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    public void addOneProduct(){
+        productRepository.addProduct("Beer");
+
+    }
+}
